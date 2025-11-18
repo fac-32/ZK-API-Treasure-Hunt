49bbe4ace39e1505eb1ed48319275e965d9feff0
Zen-333 <zenhamam56@gmail.com>
2025-11-18 11:57:49 +0000

    GET / POST

--- server.ts
+++ server.ts
@@ -2,20 +2,38 @@ import express from "express";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-import path from "path";
 
 dotenv.config();
 
 const port =  3000;
 
-
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-//const staticPath = path.join(__dirname, "");
 
 app.use(express.static(__dirname));
 app.use(express.json());
+
+var CurrentLocation = "";
+
+app.post('/InputLocation', (req, res) => {
+  CurrentLocation = req.body.CurrentLocation;
+
+  res.status(200).json({
+      message: "POST Request Called successfully",
+      locationReceived: CurrentLocation
+  });
+});
+
+app.put('/UpdateLocation', (req, res) => {
+  CurrentLocation = req.body.CurrentLocation;
+
+  res.status(200).json({
+      message: "PUT Request Called successfully",
+      locationReceived: CurrentLocation
+  });
+});
+
 
 
 app.listen(port, () => {
