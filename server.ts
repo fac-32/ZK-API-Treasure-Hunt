81ce7814c644aa24c424623693c3e6145521cf49
Kai Cheung <kaicheunga@gmail.com>
2025-11-18 12:31:01 +0000

    Add delete method function

--- server.ts
+++ server.ts
@@ -2,10 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import path from "path";
+import destinations from "./destinations.json" with { type: 'json' };
+
+let locations = destinations
 
 dotenv.config();
 
-const port =  3000;
+const port = 3000;
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +39,14 @@ app.put('/UpdateLocation', (req, res) => {
 });
 
 
+app.get("/Destinations", (request, response) => {
+  response.json(locations);
+});
+
+app.delete('/DeleteCountry', (request, response)=>{
+  locations = locations.filter(location => location.country !== CurrentLocation)  
+  response.redirect('/Destinations')
+})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
